fbb419fe77d12131fb7561f7741f43e99a1cda27
Konstantin Nazarov <knazarov@parallels.com>
2014-08-04 22:25:44 +0400

    Reimplement walk() funciton outside of ArtifactoryPath
    The walk() function doesn't really belong to ArtifactoryPath.
    It's better to reimplement it outside, so that it's possible to
    apply it to any Path object.

--- artifactory.py
+++ artifactory.py
@@ -743,12 +743,6 @@ class ArtifactoryPath(pathlib.Path, PureArtifactoryPath):
         """
         return self._accessor.is_file(self)
 
-    def listdir(self):
-        """
-        Returns a list of immediate sub-directories and files in path
-        """
-        return self._accessor.listdir(self)
-
     def is_symlink(self):
         """
         Whether this path is a symlink.
@@ -891,23 +885,25 @@ class ArtifactoryPath(pathlib.Path, PureArtifactoryPath):
         """
         return self._accessor.get_properties(self)
 
-    def walk(self, topdown=True):
-        """
-        os.walk like function to traverse the URI like a file system.
-        """
-        names = self.listdir()
-        dirs, nondirs = [], []
-        for name in names:
-            if self._accessor.is_dir(self.joinpath(name)):
-                dirs.append(name)
-            else:
-                nondirs.append(name)
-        if topdown:
-            yield self, dirs, nondirs
-        for name in dirs:
-            new_path = self.joinpath(name)
-            for x in new_path.walk():
-                yield x
-        if not topdown:
-            yield self, dirs, nondirs
+
+def walk(pathobj, topdown=True):
+    """
+    os.walk like function to traverse the URI like a file system.
+
+    The only difference is that this function takes and returns Path objects
+    in places where original implementation will return strings
+    """
+    dirs, nondirs = [], []
+    for child in pathobj.iterdir():
+        if child.is_dir():
+            dirs.append(child)
+        else:
+            nondirs.append(child)
+    if topdown:
+        yield pathobj, dirs, nondirs
+    for dir in dirs:
+        for result in walk(dir):
+            yield result
+    if not topdown:
+        yield pathobj, dirs, nondirs
 
