f8373c45f6aca9c46eac416d9d56def0290bb6d0
Konstantin Nazarov <knazarov@parallels.com>
2014-09-18 15:32:48 +0400

    Bump version to 0.1.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='artifactory',
-    version='0.1.7',
+    version='0.1.8',
     py_modules=['artifactory'],
     license='MIT License',
     description='A Python to Artifactory interface',
