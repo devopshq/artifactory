c8f9f1a7478baf28d8527c018333ed9f39ffd9b2
Konstantin Nazarov <knazarov@parallels.com>
2015-07-09 01:27:09 +0300

    Bump package version to 0.1.13

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ except ImportError:
 
 setup(
     name='artifactory',
-    version='0.1.12',
+    version='0.1.13',
     py_modules=['artifactory'],
     license='MIT License',
     description='A Python to Artifactory interface',
