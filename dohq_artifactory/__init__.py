4a1f420d6df7f8a6b3e45bdd71dd52bd98eff916
Aleksey Burov <allburov@gmail.com>
2018-03-27 13:47:46 +0700

    Init new class

---
+++ dohq_artifactory/__init__.py
@@ -0,0 +1,5 @@
+from dohq_artifactory.admin import *
+# Don't remove unused import
+from artifactory import *
+from artifactory import _ArtifactoryAccessor, _ArtifactoryFlavour, _artifactory_flavour, _artifactory_accessor
+
---
+++ dohq_artifactory/admin.py
@@ -0,0 +1,14 @@
+class User:
+    pass
+
+
+class Repo:
+    pass
+
+
+class Group:
+    pass
+
+
+class Permission:
+    pass
