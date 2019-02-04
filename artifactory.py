d054a5b81507e6dcd64c08089afa846b51c5c8a1
Aleksey Burov <allburov@gmail.com>
2019-02-04 11:35:04 +0700

    pass auth as is if auth_type is None

--- artifactory.py
+++ artifactory.py
@@ -892,21 +892,20 @@ class ArtifactoryPath(pathlib.Path, PureArtifactoryPath):
 
         # Auth section
         apikey = kwargs.get('apikey', None)
-        auth_type = kwargs.get('auth_type', tuple)
+        auth_type = kwargs.get('auth_type', None)
         if apikey is None:
             auth = kwargs.get('auth', None)
-            obj.auth = auth_type(auth)
+            obj.auth = auth if auth_type is None else auth_type(*auth)
         else:
             logging.debug('Use XJFrogApiAuth')
             obj.auth = XJFrogArtApiAuth(apikey)
 
         if obj.auth is None and cfg_entry:
-            obj.auth = auth_type(cfg_entry['username'],
-                                                    cfg_entry['password'])
+            auth = (cfg_entry['username'], cfg_entry['password'])
+            obj.auth = auth if auth_type is None else auth_type(*auth)
 
         obj.cert = kwargs.get('cert', None)
         obj.session = kwargs.get('session', None)
-
 
         if obj.cert is None and cfg_entry:
             obj.cert = cfg_entry['cert']
