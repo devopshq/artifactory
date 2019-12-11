6b11ff21aad46282812538abc9d252ecae55f438
Nikolay Yusev <nyusev@ptsecurity.com>
2019-12-11 12:52:13 +0700

    add install secrets

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     ],
     url="https://devopshq.github.io/artifactory/",
     download_url="https://github.com/devopshq/artifactory",
-    install_requires=['pathlib ; python_version<"3.4"', "requests", "python-dateutil"],
+    install_requires=['pathlib ; python_version<"3.4"', "requests", "python-dateutil", "secrets"],
     zip_safe=False,
     package_data={"": ["README.md"]},
     packages=["dohq_artifactory"],
