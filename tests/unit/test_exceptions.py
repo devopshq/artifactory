66c2629a41eae7890296e664764d4dddadcd73ea
Serban Constantin <serban.constantin@gmail.com>
2020-04-28 21:42:24 +0300

    add test for raise_errors

---
+++ tests/unit/test_exceptions.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import unittest
+import mock
+
+import requests
+
+from dohq_artifactory.admin import raise_errors
+from dohq_artifactory.exception import ArtifactoryException
+
+
+class UtilTest(unittest.TestCase):
+    def test_raise_errors(self):
+        r = requests.Response()
+        r.status_code = 400
+        type(r).text = mock.PropertyMock(return_value='asd')
+        with self.assertRaises(ArtifactoryException) as cm:
+            raise_errors(r)
+        self.assertEqual('asd', str(cm.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
