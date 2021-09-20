83b7332bbb3857db8f20dde3b5549916ffe36b21
Maksim Beliaev <beliaev.m.s@gmail.com>
2021-09-20 13:54:07 +0200

    updated test

--- tests/unit/test_exceptions.py
+++ tests/unit/test_exceptions.py
@@ -10,6 +10,7 @@ from dohq_artifactory.exception import raise_http_errors
 
 class UtilTest(unittest.TestCase):
     def test_raise_errors(self):
+        # no JSON body, just HTTP response message
         with responses.RequestsMock() as mock:
             url = "http://b.com/artifactory/"
             mock.add(responses.GET, url, status=403)
@@ -20,6 +21,21 @@ class UtilTest(unittest.TestCase):
             self.assertEqual(
                 f"403 Client Error: Forbidden for url: {url}", str(cm.exception)
             )
+
+        # real JSON body, can parse for clean message
+        with responses.RequestsMock() as mock:
+            url = "http://b.com/artifactory/"
+            mock.add(
+                responses.GET,
+                url,
+                status=403,
+                json={"errors": [{"status": 401, "message": "Bad credentials"}]},
+            )
+            resp = requests.get("http://b.com/artifactory/")
+
+            with self.assertRaises(ArtifactoryException) as cm:
+                raise_http_errors(resp)
+            self.assertEqual("Bad credentials", str(cm.exception))
 
 
 if __name__ == "__main__":
